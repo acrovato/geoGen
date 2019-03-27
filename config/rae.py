bdcc7107d6f745351b7b87e332382b9a65cded46
acrovato <39187559+acrovato@users.noreply.github.com>
2019-03-27 20:46:29 +0100

    Add multiplanform wing. Add shippable check

--- config/rae.py
+++ config/rae.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-## Onera M6 configuration module for geoGen
+## Custom rae wing configuration module for geoGen
 #
 # Adrien Crovato
 
@@ -9,13 +9,13 @@ def getParams():
     p = {}
     # Wing parameters (nP planforms for half-wing)
     p['airfPath'] = '../airfoils' # path pointing the airfoils directory (relative to this config file)
-    p['airfName'] = ['oneraM6.dat', 'oneraM6.dat'] # names of file containing airfoil (Selig formatted) data (size: nP+1)
-    p['span'] = [1.196] # span of each planform (size: nP)
-    p['taper'] = [0.562] # taper of each planform (size: nP)
-    p['sweep'] = [30] # leading edge sweep of each planform (size: nP) 
-    p['dihedral'] = [0] # dihedral angle of each planform (size: nP)
-    p['twist'] = [0, 0] # twist angle of each airfoil (size: nP+1)
-    p['rootChord'] = 0.8059 # root chord
+    p['airfName'] = ['rae2822.dat', 'rae2822.dat', 'rae2822.dat'] # names of file containing airfoil (Selig formatted) data (size: nP+1)
+    p['span'] = [.5, 2.] # span of each planform (size: nP)
+    p['taper'] = [.8, .4] # taper of each planform (size: nP)
+    p['sweep'] = [20., 20.] # leading edge sweep of each planform (size: nP) 
+    p['dihedral'] = [2., 1.] # dihedral angle of each planform (size: nP)
+    p['twist'] = [1., 0, -1.] # twist angle of each airfoil (size: nP+1)
+    p['rootChord'] = 1.0 # root chord
     p['coWingtip'] = True # cut-off wingtip (not supported yet)
     # Box
     p['xoBox'] = -3.5*p['rootChord']
